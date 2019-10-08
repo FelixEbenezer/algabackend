4c774c374c8afa099fa2c40856cf18e5ab071264
Nga <felixwhite2008@hotmail.com>
2019-10-08 12:55:23 +0200

    novo

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -56,7 +56,7 @@ public class LancamentoService {
 	private Mailer mailer;
 	
 	// actualizadao o metodo abaixo
-	@Scheduled(cron = "0 30 12 04 * * ")
+	@Scheduled(cron = "0 40 12 8 * * ")
 	public void avisarSobreLancamentosVencidos() {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Preparando envio de "
